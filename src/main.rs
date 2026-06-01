20b6b471bd285eb9b41d42619d2384956114d186
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-06-02 01:43:30 +0200

    Connect to db

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
-use axum::{Router, response::IntoResponse, routing::get};
-use serde::{Deserialize, Serialize};
+use anyhow::Result;
+use axum::{Router, routing::get};
+use sqlx::postgres::PgPoolOptions;
 use tower_http::cors::CorsLayer;
+use tracing::Level;
 
 #[tokio::main]
-async fn main() {
-    tracing_subscriber::fmt::init();
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .pretty()
+        .init();
+
+    dotenv::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
 
     let cors = CorsLayer::permissive();
 
-    let app = Router::new().layer(cors).route("/", get(root));
+    let app = Router::new()
+        .with_state(pool)
+        .layer(cors)
+        .route("/", get(root));
 
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("Listening on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     let _ = axum::serve(listener, app).await;
+
+    Ok(())
 }
 
 async fn root() -> &'static str {
