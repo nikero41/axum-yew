7f97e92171d4b46322f79791679ea35340fdfec1
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-06-02 00:59:34 +0200

    Add cors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use axum::{Router, response::IntoResponse, routing::get};
 use serde::{Deserialize, Serialize};
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let cors = CorsLayer::permissive();
+
+    let app = Router::new().layer(cors).route("/", get(root));
 
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("Listening on {}", addr);
